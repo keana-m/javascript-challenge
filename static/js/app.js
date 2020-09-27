09e23728b8879006cb707f865d0126e5229af651
Keana Mabilog <63520747+keana-m@users.noreply.github.com>
2020-09-27 18:34:58 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
-var tableData = data;
+var tableData = data;
 
-// YOUR CODE HERE!
+// Reference table body
+var tbody = d3.select("tbody");
+
+// Console.log data from data.js
+console.log(data);
+tableData.forEach((UFO_Data) => {
+  var row = tbody.append("tr");
+  Object.entries(UFO_Data).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+
+});
+
+  //Reference button
+  var button = d3.select("#filter-btn");
+
+  //Button click handler
+  button.on("click", function() {
+    console.log("Table Filtered!");
+	
+    //Clear tbody
+    tbody.html("");
+	
+    //Reference to "Enter a Date" 
+    var input = d3.select("#datetime");
+	
+    //Grab user input value
+    var userInput = input.property("value");
+	
+    //Filter Table According to userInput
+    var filterTable = tableData.filter(tableData => tableData.datetime === userInput);
+    console.log(filterTable);
+	
+    //Add filterTable to tbody to 
+    filterTable.forEach((UFO_Data) => {
+      var row = tbody.append("tr");
+      Object.entries(UFO_Data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  });
